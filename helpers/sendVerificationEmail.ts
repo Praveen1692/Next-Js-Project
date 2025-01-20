1e6e8b4dea12ec2f65babc753da0b0bf78f529f4
Praveen <praying.sharma@gmail.com>
2025-01-20 11:27:26 +0530

    Email Send

--- helpers/sendVerificationEmail.ts
+++ helpers/sendVerificationEmail.ts
@@ -1,2 +1,27 @@
 import { resend } from "@/lib/resendEmail";
 import VerificationEmail from "@/emails/VerificationEmails";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+        from:"onboarding@resend.dev",
+        to:email,
+        subject: "Next Verification Code",
+        react:<Email url=""/>,
+       
+       
+
+
+      });
+    
+    return { success: true, message: "veridication code send successfully" };
+  } catch (emailError) {
+    console.error("Error in send verification code:", emailError);
+    return { success: false, message: "Failed to send verification code " };
+  }
+}
