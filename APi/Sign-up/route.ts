490173fb5eef2a721592d949165b0d1ba7c9f980
Praveen <praying.sharma@gmail.com>
2025-01-21 09:27:21 +0530

    new user

--- APi/Sign-up/route.ts
+++ APi/Sign-up/route.ts
@@ -7,23 +7,54 @@ export async function POST(request: Request) {
   await dbConnect();
 
   try {
-   const {username,email,password}= await request.json();
-   UserModel.findOne({
-    username,
-    isVerified: true,
-   })
+    const { username, email, password } = await request.json();
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username already exists",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
 
+    const existingUserByEmail = await UserModel.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+    if (existingUserByEmail) {
+      return Response.json(
+        {
+          success: false,
+          message: "Email already exists",
+        },
+        {
+          status: 400,
+        }
+      );
+    } else {
+      const hashPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      // set Hours in expiryDate
+      expiryDate.setHours(expiryDate.getHours() + 1);
 
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashPassword,
+        verifyCode,
+        verificationCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessages: true,
+        messages: [],
+      });
 
-
-
-
-
-
-
-
-
-
+      await newUser.save();
+    }
   } catch (error) {
     console.log("Error registering user", error);
     return Response.json(
